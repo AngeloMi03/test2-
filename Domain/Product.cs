4a9898583dead4db507815250ba33eb66c497da5
AngeloMi03 <142218795+AngeloMi03@users.noreply.github.com>
2024-03-28 17:43:58 +0100

    Update Product.cs

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Domain;
-
-public class Product
+namespace Domain
 {
-    public string Name { get; set; }
-    public string Matricule { get; set; }
-    [Key]
-    public Guid Slug { get;  set; }
-    public DateTime Date_Create { get;  set; }
-    public DateTime Date_Edit { get;  set; }
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Matricule { get; set; }
+        [Key]
+        public Guid Slug { get;  set; }
+        public DateTime Date_Create { get;  set; }
+        public DateTime Date_Edit { get;  set; }
+    }
 }
+
+
